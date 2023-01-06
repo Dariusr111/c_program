410048f5574c2a694a31b9fb88ef656973428fdd
dariusr <darius.r111@gmail.com>
2023-01-06 13:50:32 +0200

    Folder examples added

---
+++ examples/two-integers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ int main()
+ {
+  int num1;
+  int num2;
+  int total;
+  printf("Enter first number : ");
+  scanf("%d",&num1);
+  printf("Enter second number : ");
+  scanf("%d",&num2);
+  total = num2 + num1;
+  printf("Total is =  %d",total);
+  return 0;
+ }
